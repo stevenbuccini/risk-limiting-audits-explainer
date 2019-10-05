51658512cbf5716b0c42200dd13d52e8a327243b
Steven Buccini <steven@stevenbuccini.com>
2019-10-05 13:38:10 -0400

    Work on deploying to GH pages

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,8 +1,31 @@
 /* global d3 */
-function resize() {}
+function resize() { }
 
 function init() {
   console.log('Make something awesome!');
+
+  // const scale = 22;
+  // let root = d3.select("#simulation1 svg");
+
+  // for (let x = 0; x < 25; x++) {
+  //   for (let y = 0; y < 10; y++) {
+  //     root.append('rect')
+  //       .attr('transform', `translate(${x * scale}, ${y * scale})`)
+  //       .attr('width', scale)
+  //       .attr('height', scale)
+  //       .attr('fill', "white")
+  //       .attr('stroke', "gray");
+  //   }
+  // }
+
+  // let circle = root.append('circle')
+  //   .attr('class', "draggable")
+  //   .attr('r', scale * 0.75)
+  //   .attr('fill', "hsl(0,50%,50%)"
+
+
 }
+
+
 
 export default { init, resize };
